c15b745204c506ef0bb10fe5522523b485e8703f
Jamel Travis <jameltravis@gmail.com>
2018-02-22 22:05:50 -0500

    finished task 1

---
+++ pypaths.py
@@ -0,0 +1,7 @@
+"""Python paths.
+
+Actual Python path: #!/Users/jtza/anaconda3/envs/is210/bin/python
+
+REMEMBER TO REPLACE BEFORE SUBMITTING:
+Class path: #!/usr/bin/env python
+"""
--- task_01.py
+++ task_01.py
@@ -1,9 +1,25 @@
-#!/usr/bin/env python
+#!/Users/jtza/anaconda3/envs/is210/bin/python
 # -*- coding: utf-8 -*-
 """This module provides a function that knows what you mean"""
 
 
 def know_what_i_mean(wink, numwink=2):
+    """Creates a funny string.
+    Args:
+        wink (str): Arg to be multiplied by numwink and striped of spaces
+        numwink (int, optional): Arg number of times arg1 and str 'nudge' are multiplied. Default is 2.
+
+    Returns:
+        str: 'Know what I mean <var1>, [<var2>]':
+    
+    Examples:
+    
+        >>> know_what_i_mean('toot')
+        'Know what I mean? toottoot, nudge nudge'
+
+        >>> know_what_i_mean('toot', 3)
+        'Know what I mean? toottoottoot, nudge nudge nudge
+    """
     winks = (wink * numwink).strip()
     nudges = ('nudge ' * numwink).strip()
     retstr = 'Know what I mean? {}, {}'.format(winks, nudges)
